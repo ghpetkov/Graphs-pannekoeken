0489eacd79e2be8c9c9cee1984b5ef5a7d0b875a
ghpetkov <ghpetkov@gmail.com>
2016-05-14 13:57:48 +0100

    fg_symmGraph_10.m
    simply test if I could type something in this "unfriendly" environment...
    lets obtain a symmetric graph from directed one - simple task!

---
+++ fg_symmGraph_10.m
@@ -0,0 +1,15 @@
+function xU = fg_symmGraph_10( x )
+% lets obtain a symmetric graph from directed one
+% ----- Summary of this function goes here
+% Prepare symmetric matrix - undirected graph from directed original
+% We suppose original Directed SIMPLE graph, i.e. we have zeroes on the
+% main diagonal (graph without self loops and multiple edges)
+% INPUT:
+%   x - original Directed graph connectivity matrix (Weighted/Unweighted)
+% OUTPUT:
+%   xU- induced UnDirected graph connectivity matrix
+%   Detailed explanation goes here
+    y = triu(x);    z = triu(x');
+    U = max(y,z);   xU = U+U';
+return
+end
